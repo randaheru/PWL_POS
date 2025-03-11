211cac660121a7e4c7065d7ad68b856cefb5689e
= <=>
2025-03-11 13:53:02 +0700

    PERUBAHAN PRAKTIKUM 4

--- database/seeders/PenjualanSeeder.php
+++ database/seeders/PenjualanSeeder.php
@@ -2,23 +2,16 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class PenjualanSeeder extends Seeder
 {
+    /**
+     * Run the database seeds.
+     */
     public function run(): void
     {
-        $data = [];
-        for ($i = 1; $i <= 10; $i++) {
-            $data[] = [
-                'penjualan_id' => $i,
-                'user_id' => rand(1, 3),
-                'tanggal' => now()->subDays(rand(1, 30)),
-                'total_harga' => 0, // Akan dihitung di t_penjualan_detail
-            ];
-        }
-
-        DB::table('t_penjualan')->insert($data);
+        //
     }
 }
